1d6322a19a458763c514a0b0700e048c0033638f
ghtf <gfdgdgd@re.com>
2018-01-17 23:35:39 +0800

    刷新

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <el-button size="mini" class="refresh_echart" type="default" @click="get_echarts_data">
+      <i class="el-icon-refresh"></i>
+    </el-button>
     <div id="main" style="width: 100%;height:500px;"></div>
   </div>
 </template>
@@ -13,24 +16,29 @@ export default {
     }
   },
   created () {
-    
     this.get_echarts_data();
   },
   methods: {
     get_echarts_data(){
       getEcharts().then(
         (resData) => {
-         
           this.echart_data = resData.results;
-          this.set_chart(this.echart_data[0].active_times, this.echart_data[0].active_count);
+          this.set_chart(this.echart_data[0].active_times, this.echart_data[0].active_count);  
+          this.set_interval();
         }
       )
     },
     set_chart(xdata, ydata){
       var myChart = echarts.init(document.getElementById('main'));
       var option = {
+          title: {
+              text: '在线人数监控图'
+          },
           tooltip: {
               trigger: 'axis'
+          },
+          legend: {
+              data:['人数']
           },
           xAxis:  {
               type: 'category',
@@ -47,7 +55,7 @@ export default {
           },
           series: [
               {
-                  name:'',
+                  name:'人数',
                   type:'line',
                   data:ydata
               }
@@ -59,8 +67,32 @@ export default {
       window.addEventListener("resize", function () {
           myChart.resize();
       })
+    },
+    set_interval(){
+      var self = this;
+      var interval = window.setInterval(function(){
+        getEcharts().then(
+          (resData) => {
+            self.echart_data = resData.results;
+            self.set_chart(self.echart_data[0].active_times, self.echart_data[0].active_count);  
+          }
+        )
+      
+      }, 60*1000*10)
     }
   }
 }
 </script>
+<style>
+.refresh_echart{
+  float: right;
+  margin: 0 10% 0 0;
+  cursor: pointer;
+}
+#main{
+  clear: both;
+}
+
+</style>
+
 
